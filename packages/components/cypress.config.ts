8cc2ff5bcce3644e9cc530be6a6488422243036d
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2022-10-14 11:32:22 +0200

    test: add performance tests

--- packages/components/cypress.config.ts
+++ packages/components/cypress.config.ts
@@ -13,12 +13,25 @@ export default defineConfig({
 
   lighthouse: {
     options: {
-      formFactor: 'desktop',
-      screenEmulation: {
-        deviceScaleRatio: 1,
-        mobile: false,
-        disable: false,
+      formFactor: 'mobile',
+      skipAudits: ['uses-http2'],
+      throttling: {
+        rttMs: 40,
+        throughputKbps: 10 * 1024,
+        cpuSlowdownMultiplier: 1,
+        requestLatencyMs: 0, // 0 means unset
+        downloadThroughputKbps: 0,
+        uploadThroughputKbps: 0,
       },
+      screenEmulation: {
+        mobile: false,
+        width: 1350,
+        height: 940,
+        deviceScaleFactor: 1,
+        disabled: false,
+      },
+      emulatedUserAgent:
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4695.0 Safari/537.36 Chrome-Lighthouse',
     },
   },
 
