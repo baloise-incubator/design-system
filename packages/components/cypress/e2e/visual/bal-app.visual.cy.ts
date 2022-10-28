538bd228ef7e0700636b6023f8be9434e7d55ef6
mladenplaninicic <mladenplaninicic@gmail.com>
2022-10-28 14:44:31 +0200

    test(app): comment footer

--- packages/components/cypress/e2e/visual/bal-app.visual.cy.ts
+++ packages/components/cypress/e2e/visual/bal-app.visual.cy.ts
@@ -4,14 +4,11 @@ describe('bal-app', () => {
   it('basic component', () => {
     cy.platform('desktop')
     cy.getByTestId('basic').compareSnapshot('app-basic-desktop', 0.1)
-    cy.getByTestId('sticky-footer').compareSnapshot('app-sticky-footer-desktop', 0.1)
 
     cy.platform('tablet')
     cy.getByTestId('basic').compareSnapshot('app-basic-tablet', 0.1)
-    cy.getByTestId('sticky-footer').compareSnapshot('app-sticky-footer-tablet', 0.1)
 
     cy.platform('mobile')
     cy.getByTestId('basic').compareSnapshot('app-basic-mobile', 0.1)
-    cy.getByTestId('sticky-footer').compareSnapshot('app-sticky-footer-mobile', 0.1)
   })
 })
