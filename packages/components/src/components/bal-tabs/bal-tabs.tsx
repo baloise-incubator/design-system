4b6193726d9772c20d1eb7705ee1f4f93b1ddadb
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2023-02-28 12:05:33 +0100

    chore: fix lint issue

--- packages/components/src/components/bal-tabs/bal-tabs.tsx
+++ packages/components/src/components/bal-tabs/bal-tabs.tsx
@@ -1,6 +1,6 @@
 import { Component, Host, h, Element, State, Event, EventEmitter, Method, Prop, Watch, Listen } from '@stencil/core'
 import { areArraysEqual } from '@baloise/web-app-utils'
-import { debounceEvent, deepReady, isDescendant, parentDidAnimate, raf, transitionEndAsync } from '../../utils/helpers'
+import { debounceEvent, deepReady, parentDidAnimate, raf, transitionEndAsync } from '../../utils/helpers'
 import { BalTabOption } from './bal-tab.type'
 import { watchForTabs } from './utils/watch-tabs'
 import { Props, Events } from '../../types'
