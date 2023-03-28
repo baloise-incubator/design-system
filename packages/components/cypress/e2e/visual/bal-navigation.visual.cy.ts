ca8265d83c68fdbc5315916fea428dc906af97ca
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2023-03-28 08:48:34 +0200

    chore: update visual test

--- packages/components/cypress/e2e/visual/bal-navigation.visual.cy.ts
+++ packages/components/cypress/e2e/visual/bal-navigation.visual.cy.ts
@@ -55,7 +55,7 @@ describe.skip('bal-navigation', () => {
   }
 })
 
-describe('bal-navigation-colors', () => {
+describe.skip('bal-navigation-colors', () => {
   testNavigationOnDesktop('widescreen')
 
   function testNavigationOnDesktop(platform: Platforms) {
