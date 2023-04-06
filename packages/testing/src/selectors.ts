f6273df7ee372fdf47b59832b70ec0bbf112dc7b
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2023-04-06 12:09:58 +0200

    chore: adjust conflict and fix style conflict

--- packages/testing/src/selectors.ts
+++ packages/testing/src/selectors.ts
@@ -8,9 +8,6 @@ export const byTestId = (testId: string): string => `[data-testid="${testId}"]`
  */
 export const selectors = {
   accordion: {
-    /**
-     * Comments
-     */
     button: byTestId('bal-accordion-button'),
     content: byTestId('bal-accordion-content'),
   },
